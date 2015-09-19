54c951de02f770569fa5d6a7d87fe4fd77ee4735
Tomasz Pietrek <tomasz.pietrek@neo9.pl>
2015-09-19 18:38:04 +0200

    main: add http server with gorilla mux

--- main.go
+++ main.go
@@ -1,8 +1,21 @@
 package main
 
-import ()
+import (
+	"fmt"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+)
 
 func main() {
 
 	loadConfig()
+
+	r := mux.NewRouter()
+
+	// start HTTP server
+	fmt.Println("starting server on port:", cfg.Port)
+	if err := http.ListenAndServe(fmt.Sprintf(":%d", cfg.Port), r); err != nil {
+		log.Fatal("error while starting http server:", err)
+	}
 }
