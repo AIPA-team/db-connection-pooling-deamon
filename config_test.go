5452332982c71ba34652df3421e7123725709722
Tomasz Pietrek <tomasz.pietrek@neo9.pl>
2015-09-26 15:06:00 +0200

    config: add database structs and tests

--- config_test.go
+++ config_test.go
@@ -1,15 +1,40 @@
 package main
 
 import (
+	"fmt"
 	"testing"
 )
 
 func TestLoadConfig(t *testing.T) {
 	cfgFile = "test.yml"
 	port := 8000
+	password := "test"
+	host := "localhost"
+	dbName := "test"
+	user := "test"
 	loadConfig()
-	if cfg.Port != 8000 {
+	if cfg.Port != port {
 		t.Errorf("expected port %d, got %d", port, cfg.Port)
 	}
-	t.Log("listening on:", cfg.Port)
+	if cfg.DB.Password != password {
+		t.Errorf("expected password %v, got %v", password, cfg.Password)
+	}
+	if cfg.DB.Host != host {
+		t.Errorf("expected password %v, got %v", host, cfg.Host)
+	}
+	if cfg.DB.DBname != dbName {
+		t.Errorf("expected password %v, got %v", dbName, cfg.DBname)
+	}
+	if cfg.DB.User != user {
+		t.Errorf("expected password %v, got %v", user, cfg.User)
+	}
+}
+
+func TestDB_connString(t *testing.T) {
+	db := DB{DBname: "test", Password: "test", Host: "localhost", User: "test"}
+	connString := fmt.Sprintf("host=%s password=%s dbname=%s user=%s", db.Host, db.Password, db.DBname, db.User)
+	db.setConnString()
+	if db.connString != connString {
+		t.Errorf("expected connString o be  %v, got %v", connString, cfg.connString)
+	}
 }
--- main.go
+++ main.go
@@ -1,10 +1,20 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"github.com/gorilla/mux"
+	_ "github.com/lib/pq"
 	"log"
 	"net/http"
+)
+
+var (
+	// global variables that hold config related data
+	cfg     *config
+	cfgFile = "./config.yml" // default path to config file
+	// global pointer to db connection
+	db *sql.DB
 )
 
 func main() {
