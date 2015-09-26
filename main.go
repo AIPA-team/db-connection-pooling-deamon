1ad9f025e9c2c88ae759bb8bbc32ae618b5df68f
Tomasz Pietrek <tomasz.pietrek@neo9.pl>
2015-09-26 20:47:32 +0200

    main: switch to sqlx

--- main.go
+++ main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"database/sql"
 	"fmt"
 	"github.com/gorilla/mux"
+	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
 	"log"
 	"net/http"
@@ -14,14 +14,24 @@ var (
 	cfg     *config
 	cfgFile = "./config.yml" // default path to config file
 	// global pointer to db connection
-	db *sql.DB
+	db *sqlx.DB
 )
 
 func main() {
 
+	var err error
+
 	loadConfig()
 
+	//conenct to api databases
+	db, err = sqlx.Open("postgres", cfg.DB.connString+" sslmode=disable")
+	if err != nil {
+		log.Println("error while connecting to database:", err)
+		panic(err)
+	}
+
 	r := mux.NewRouter()
+	r.HandleFunc("/query", query)
 
 	// start HTTP server
 	fmt.Println("starting server on port:", cfg.Port)
