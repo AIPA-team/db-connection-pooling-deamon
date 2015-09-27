148baae7f8bc4037280902d6f3153ee84c9b3032
Tomasz Pietrek <tomasz.pietrek@neo9.pl>
2015-09-27 20:51:47 +0200

    tests: refactor and improve httpHandler tests/

--- httpHandlers_test.go
+++ httpHandlers_test.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"fmt"
-	"github.com/jmoiron/sqlx"
 	"net/http"
 	"net/http/httptest"
 	"strings"
@@ -22,19 +20,18 @@ func TestQuery(t *testing.T) {
 	POSTBody := `{"query": "SELECT * from test where age > $1", "params": ["20"]}`
 	expected := `[{"age":30,"firstname":"Tomasz","id":1,"lastname":"Pietrek"},{"age":32,"firstname":"Artur","id":2,"lastname":"Gawroński"},{"age":23,"firstname":"Łukasz","id":3,"lastname":"Żarnowiecki"}]`
 
-	db, err = sqlx.Open("postgres", cfg.DB.connString+" sslmode=disable")
-	if err != nil {
-		fmt.Println("error while connecting to database:", err)
-		panic(err)
-	}
-
-	req, err := http.NewRequest("POST", "/query", strings.NewReader(POSTBody))
+	req, err := http.NewRequest("POST", "", strings.NewReader(POSTBody))
 	if err != nil {
 		t.Error(err)
 	}
+	req.RemoteAddr = "127.0.0.1:8000"
 
 	w := httptest.NewRecorder()
 	query(w, req)
+
+	if w.Code != 200 {
+		t.Errorf("expected 200, got %v", w.Code)
+	}
 
 	if w.Body.String() != expected {
 		t.Errorf("expected \n %v \n got \n %v", expected, w.Body.String())
